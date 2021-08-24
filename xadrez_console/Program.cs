73127832db12eb54e49f713fdcd254dbe5b86d6f
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2021-08-24 16:33:44 -0300

    Melhorando a impressao do tabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -13,7 +13,7 @@ namespace xadrez_console
                 TabuleiroXadrez tab = new TabuleiroXadrez(8, 8);
 
                 tab.ColocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-                tab.ColocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 0));
+                
                 tab.ColocarPeca(new Rei(tab, Cor.Branco), new Posicao(3, 4));
 
                 Tela.ImprimirTabuileiro(tab);
--- xadrez_console/Tela.cs
+++ xadrez_console/Tela.cs
@@ -13,14 +13,31 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.Peca(i, j) == null)
                         Console.Write("- ");
                     else
-                        Console.Write(tab.Peca(i, j) + " ");
+                        ImprimirPeca(tab.Peca(i, j));
                 }
                 Console.WriteLine();
+            }
+            Console.Write("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branco)
+            {
+                Console.Write(peca + " ");
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(peca + " ");
+                Console.ForegroundColor = aux;
             }
         }
     }
